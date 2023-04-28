d42fc5d6ff65f4582a46bfac5a3af81d69a5e7e4
Davi <davisilvaqueiroz@outlook.com>
2023-04-28 14:35:15 -0300

    box de task estilizada

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -5,9 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+export class HomeComponent  {
+
 
 }
